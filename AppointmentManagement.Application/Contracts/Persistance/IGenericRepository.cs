294d5d5cb043d956e3d560e5327f78dc8b909e7d
RahulS290600 <mailatrahuls@gmail.com>
2024-09-03 09:49:00 +0530

    initial commit

--- AppointmentManagement.Application/Contracts/Persistance/IGenericRepository.cs
+++ AppointmentManagement.Application/Contracts/Persistance/IGenericRepository.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace AppointmentManagement.Application.Contracts.Persistance
 {
-    public interface IGenericRepository< T> where T : class
+    public interface IGenericRepository<T> where T : class
     {
-        Task<List<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task<T> CreateAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
 
     }
 
